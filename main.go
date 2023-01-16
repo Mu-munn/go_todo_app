369aef299ce887b4124a27bce3886b07be90dc94
Mu-munn <rpgmukai333@gmail.com>
2023-01-16 23:10:12 +0900

    configの作成

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/Mu-munn/go_todo_app/config"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -26,11 +27,18 @@ func main() {
 		os.Exit(1)
 	}
 }
-func run(ctx context.Context, l net.Listener) error {
+func run(ctx context.Context) error {
+	cfg, err := config.New()
+	if err != nil {
+		return err
+	}
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("faild to listen port %d: %v", cfg.Port, err)
+	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	fmt.Printf("start with %v", url)
 	s := &http.Server{
-		// Addrフィールドではなく、引数で取るnet.Listenerを使用したい。
-		// Addr: ":18080",
-
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
@@ -39,7 +47,7 @@ func run(ctx context.Context, l net.Listener) error {
 	eg, ctx := errgroup.WithContext(ctx)
 	// 別ゴルーチンでHTTPサーバーを起動する
 	eg.Go(func() error {
-
+		
 		if err := s.Serve(l); err != nil &&
 			err != http.ErrServerClosed {
 			log.Printf("faild to start http server : %+v", err)
